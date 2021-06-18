c9b87a6b0a69149f29bb626f216808582ff11121
Naser Abdulrahman <abdul.naser724@gmail.com>
2021-06-18 19:39:46 -0400

    imported discord.ext package + Added event function that prints the voice channel a user joins and their id.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import os
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix='p!')
+
 
 
 @client.event
@@ -16,7 +18,15 @@ async def on_message(message):
 
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    channel = after.channel
+    bot_connection = member.guild.voice_client
+    print(str(channel) + "    " + str(member.id))
+
 client.run(os.getenv('TOKEN'))
 
-#Simple bot that replies to hello messages.
-#Future methods will be added below
+# Simple bot that replies to hello messages.
+# Future methods will be added below
